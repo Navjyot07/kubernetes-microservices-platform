b5a21718d551a6b82f65849dff7e17d9d3524da5
Navjyot Sakhalkar <navjyot@mortgagekart.com>
2026-04-13 12:42:22 +0530

    Added CPU, IO and Memory bound API's for testing Scaling

--- app/main.py
+++ app/main.py
@@ -1,7 +1,40 @@
 from fastapi import FastAPI
+import time
+import requests
+
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"message": "Hello from Kubernetes"}
+
+
+# CPU-bound API
+@app.get("/cpu")
+def cpu_task(n: int = 5000000):
+    total = 0
+    for i in range(n):
+        total += i
+    return {"result": total}
+
+
+# IO-bound API (simulate DB/API call)
+@app.get("/io")
+def io_task():
+    time.sleep(2)  # simulate DB delay
+    return {"message": "IO task completed"}
+
+
+# External API call (real IO)
+@app.get("/external")
+def external_call():
+    res = requests.get("https://jsonplaceholder.typicode.com/posts/1")
+    return res.json()
+
+
+# Memory-bound API
+@app.get("/memory")
+def memory_task(size: int = 10000000):
+    data = ["x"] * size
+    return {"message": f"Allocated {size} items"}
